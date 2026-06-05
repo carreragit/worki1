e390f00ed86f50f447df9c102bfffdf9a5a89921
carreragit <jos.carrera@duocuc.cl>
2026-06-05 00:10:30 -0400

    Fix de conflictos para ajustarse con testing

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { UserProvider }            from './src/context/UserContext';
-import LoginScreen                 from './src/screens/LoginScreen';
-import RegisterScreen              from './src/screens/RegisterScreen';
-import TabNavigator                from './src/navigation/TabNavigator';
-import PerfilTecnicoScreen         from './src/screens/PerfilTecnicoScreen';
-import CrearSolicitudScreen        from './src/screens/CrearSolicitudScreen';
-import ActivarTrabajadorScreen     from './src/screens/ActivarTrabajadorScreen';
-import DetalleSolicitudScreen      from './src/screens/DetalleSolicitudScreen';
-import CalificarScreen             from './src/screens/CalificarScreen';
-import ChatScreen                  from './src/screens/ChatScreen';
-import WorkerProfileScreen         from './src/screens/WorkerProfileScreen';
-import ClientProfileScreen         from './src/screens/ClientProfileScreen';
+import { UserProvider } from './src/context/UserContext';
+import LoginScreen          from './src/screens/LoginScreen';
+import RegisterScreen       from './src/screens/RegisterScreen';
+import TabNavigator         from './src/navigation/TabNavigator';
+import PerfilTecnicoScreen  from './src/screens/PerfilTecnicoScreen';
+import CrearSolicitudScreen      from './src/screens/CrearSolicitudScreen';
+import ActivarTrabajadorScreen  from './src/screens/ActivarTrabajadorScreen';
+import DetalleSolicitudScreen   from './src/screens/DetalleSolicitudScreen';
+import CalificarScreen          from './src/screens/CalificarScreen';
+// Pantalla de chat en tiempo real entre cliente y trabajador (Task 1)
+import ChatScreen               from './src/screens/ChatScreen';
+import WorkerProfileScreen      from './src/screens/WorkerProfileScreen';
+import ClientProfileScreen      from './src/screens/ClientProfileScreen';
 
 const Stack = createStackNavigator();
 
@@ -26,17 +26,18 @@ export default function App() {
         <UserProvider>
           <NavigationContainer>
             <Stack.Navigator id="StackPrincipal" initialRouteName="Login" screenOptions={{ headerShown: false }}>
-              <Stack.Screen name="Login"             component={LoginScreen} />
-              <Stack.Screen name="Registro"          component={RegisterScreen} />
-              <Stack.Screen name="Tabs"              component={TabNavigator} />
-              <Stack.Screen name="PerfilTecnico"     component={PerfilTecnicoScreen} />
-              <Stack.Screen name="CrearSolicitud"    component={CrearSolicitudScreen} />
-              <Stack.Screen name="ActivarTrabajador" component={ActivarTrabajadorScreen} />
-              <Stack.Screen name="DetalleSolicitud"  component={DetalleSolicitudScreen} />
-              <Stack.Screen name="Calificar"         component={CalificarScreen} />
-              <Stack.Screen name="Chat"              component={ChatScreen} />
-              <Stack.Screen name="PerfilTrabajador"  component={WorkerProfileScreen} />
-              <Stack.Screen name="PerfilCliente"     component={ClientProfileScreen} />
+              <Stack.Screen name="Login"           component={LoginScreen} />
+              <Stack.Screen name="Registro"        component={RegisterScreen} />
+              <Stack.Screen name="Tabs"            component={TabNavigator} />
+              <Stack.Screen name="PerfilTecnico"   component={PerfilTecnicoScreen} />
+              <Stack.Screen name="CrearSolicitud"     component={CrearSolicitudScreen} />
+              <Stack.Screen name="ActivarTrabajador"  component={ActivarTrabajadorScreen} />
+              <Stack.Screen name="DetalleSolicitud"   component={DetalleSolicitudScreen} />
+              <Stack.Screen name="Calificar"          component={CalificarScreen} />
+              {/* Ruta de chat: accesible desde DetalleSolicitudScreen cuando estado === ACEPTADA (Task 1) */}
+              <Stack.Screen name="Chat"               component={ChatScreen} />
+              <Stack.Screen name="PerfilTrabajador"   component={WorkerProfileScreen} />
+              <Stack.Screen name="PerfilCliente"      component={ClientProfileScreen} />
             </Stack.Navigator>
           </NavigationContainer>
         </UserProvider>
