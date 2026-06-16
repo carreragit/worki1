111c84da85f755a95bc266aacfe6609a754d4b68
Naomi <villarroeel.na@gmail.com>
2026-06-16 15:14:04 -0400

    feat(interaction-service): registrar interceptor JWT en WebSocketConfig

--- backend/interaction-service/src/main/java/com/worki/interaction/config/WebSocketConfig.java
+++ backend/interaction-service/src/main/java/com/worki/interaction/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.worki.interaction.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
@@ -9,6 +11,9 @@ import org.springframework.web.socket.config.annotation.*;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private WebSocketAuthInterceptor authInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -23,5 +28,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app");
         // Los mensajes enviados a /topic/... son distribuidos a todos los suscriptores
         registry.enableSimpleBroker("/topic");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(authInterceptor);
     }
 }
